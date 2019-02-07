6c35aa00840bc5996fb46a2a3ecd940cdb5f9ba6
Stanley Regnard <sregnard@stanleys-air.home>
2019-02-07 12:59:34 +0100

    main_test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 17:26:00 by sregnard          #+#    #+#             */
-/*   Updated: 2019/02/01 15:35:21 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/02/05 17:56:56 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,7 @@
 
 int	main(int ac, char **av)
 {
-	char a = 'c';
-	char *s = "Hello";
-	
-	printf("printf %%p\t: %p\n", (void *)s);
-	ft_printf("ft_printf %%p\t: %p\n\n", (void *)s);
-
-	printf("printf %%#lx\t: %#lx\n", (unsigned long)s);
-	ft_printf("ft_printf %%#lx\t: %#lx\n\n", (unsigned long)s);
-
-	printf("printf %%x\t: %x\n", (unsigned int)s);
-	ft_printf("ft_printf %%x\t: %x\n\n", (unsigned int)s);
-
-	printf("printf %%o\t: %o\n", (unsigned int)s);
-	ft_printf("ft_printf %%o\t: %o\n\n", (unsigned int)s);
-
+	printf("%-9s: %-5%\n", "printf");
+	ft_printf("%-9s: %-5%\n", "ft_printf");
 	return (0);
 }
