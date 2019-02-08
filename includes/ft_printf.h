e65f9992ac1c48a5048482379ab4a46288b84ac6
Stanley Regnard <sregnard@stanleys-air.home>
2019-02-08 18:04:12 +0100

    includes

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 17:20:07 by sregnard          #+#    #+#             */
-/*   Updated: 2019/02/08 16:12:16 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/02/08 17:58:21 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,10 @@
 #include <stdarg.h>
 
 #define PF_BUFF_SIZE	256
+
+/*
+**	----------------------------------------------------------------------
+*/
 
 typedef struct	s_printf
 {
@@ -31,13 +35,17 @@ typedef struct	s_printf
 	unsigned int	written;
 }		t_printf;
 
+/*
+**	----------------------------------------------------------------------
+*/
+
 enum	e_printf_flags
 {
 	FLAG_HASH = (1 << 0),
 	FLAG_PLUS = (1 << 1),
 	FLAG_SPACE = (1 << 2),
 	FLAG_LEFT_ALIGN = (1 << 3),
-	FLAG_RIGHT_ALIGN = (1 << 4),
+	FLAG_0 = (1 << 4),
 	FLAG_WIDTH = (1 << 5),
 	FLAG_PRECISION = (1 << 6),
 	FLAG_CHAR = (1 << 7),
@@ -49,21 +57,37 @@ enum	e_printf_flags
 	FLAG_LONG_DOUBLE = (1 << 13),
 };
 
-int	ft_printf(const char *format, ...);
-
 /*
-**	pf_parse_args
+**	----------------------------------------------------------------------
 */
 
+int	ft_printf(const char *format, ...);
 int	pf_buffer(t_printf *p, const char *s, size_t len);
+int	pf_padding(t_printf *p, size_t len);
+
+/*
+**	----------------------------------------------------------------------
+*/
+
 int	pf_parse_args(t_printf *p);
-int	pf_strlen_until(const char *s, char c);
-int	pf_strlen(const char *s);
+
+/*
+**	----------------------------------------------------------------------
+*/
+
 int 	pf_putchar(t_printf *p, const char c);
 int	pf_putstr(t_printf *p, const char *s);
-int	pf_atoi(const char *s);
+
+/*
+**	----------------------------------------------------------------------
+*/
+
+int	pf_putaddr(t_printf *p, unsigned int addr);
 int	pf_putnbr(t_printf *p, int nbr, unsigned int base);
 int	pf_putnbr_u(t_printf *p, unsigned int nbr, unsigned int base);
-int	pf_putaddr(t_printf *p, unsigned int addr);
+
+/*
+**	----------------------------------------------------------------------
+*/
 
 #endif
