272a30ce22d765a8cacee60a317fb9f180cbb0f2
Stanley Regnard <sregnard@stanleys-air.home>
2019-02-08 16:16:53 +0100

    main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 17:26:00 by sregnard          #+#    #+#             */
-/*   Updated: 2019/02/07 15:07:25 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/02/08 16:14:57 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,38 @@
 
 int	main(int ac, char **av)
 {
-	char	*s;
-	size_t	len;
-
-	len = 50;
-	s = (char *)malloc(sizeof(char) * len + 1);
-	ft_memset(s, 'a', len);
-	s[len] = '\0';
-//	printf("%-9s: %-5%\n", "printf");
-//	ft_printf("%-9s: %-5%\n", "ft_printf");
-	printf("%-10s :\n", "printf");
-	printf("{%0+10d}", 42);
+	printf("printf :\n");
+	printf("%d", -347);
 	printf("\n");
-	ft_printf("%-10s :\n", "ft_printf");
-	ft_printf("{%0+10d}", 42);
+	printf("ft_printf :\n");
+	ft_printf("%d", -347);
+	printf("\n");
+	
+	printf("\n");
+
+	printf("printf :\n");
+	printf("%o", -347);
+	printf("\n");
+	printf("ft_printf :\n");
+	ft_printf("%o", -347);
+	printf("\n");
+	
+	printf("\n");
+
+	printf("printf :\n");
+	printf("%x", -347);
+	printf("\n");
+	printf("ft_printf :\n");
+	ft_printf("%x", -347);
+	printf("\n");
+	
+	printf("\n");
+
+	printf("printf :\n");
+	printf("%X", -347);
+	printf("\n");
+	printf("ft_printf :\n");
+	ft_printf("%X", -347);
 	printf("\n");
 	return (0);
 }
