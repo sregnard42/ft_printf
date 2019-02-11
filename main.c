1277c0eac6db6e2ff53dfdd5efe2f13a6889694b
Stanley Regnard <sregnard@stanleys-air.home>
2019-02-11 17:58:41 +0100

    main.c

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 17:26:00 by sregnard          #+#    #+#             */
-/*   Updated: 2019/02/11 15:13:57 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/02/11 17:51:57 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 #include <stdio.h>
 #include <limits.h>
+#include <float.h>
 
 int	main(int ac, char **av)
 {
@@ -21,31 +22,13 @@ int	main(int ac, char **av)
 
 	i = 0;
 
-	d = 6.2222222222222222;
+	d = LDBL_MAX;
 	printf("\nTest #%d :\n", ++i);
 	printf("| %-10s |\n", "printf");
-	printf("| %10.16Lf |", d);
+	printf("%.0p, %.p", 0, 0);
 	printf("\n");
 	ft_printf("| %-10s |\n", "ft_printf");
-	ft_printf("| %10.16Lf |", d);
-	ft_printf("\n");
-
-	d = 6.4444444444444444;
-	printf("\nTest #%d :\n", ++i);
-	printf("| %-10s |\n", "printf");
-	printf("| %10.16Lf |", d);
-	printf("\n");
-	ft_printf("| %-10s |\n", "ft_printf");
-	ft_printf("| %10.16Lf |", d);
-	ft_printf("\n");
-
-	d = 6.2222;
-	printf("\nTest #%d :\n", ++i);
-	printf("| %-10s |\n", "printf");
-	printf("| %10.16Lf |", d);
-	printf("\n");
-	ft_printf("| %-10s |\n", "ft_printf");
-	ft_printf("| %10.16Lf |", d);
+	ft_printf("%.0p, %.p", 0, 0);
 	ft_printf("\n");
 	return (0);
 }
