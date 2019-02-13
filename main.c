d7f3b92c1ab0dc4e55e63040df730a765c9743d2
Stanley REGNARD <sregnard@e2r6p9.42.fr>
2019-02-13 15:53:41 +0100

    main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 17:26:00 by sregnard          #+#    #+#             */
-/*   Updated: 2019/02/11 19:17:38 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/02/13 15:40:08 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,38 @@
 int	main(int ac, char **av)
 {
 	unsigned int	i;
-	long double	d;	
+	unsigned long	u;
+	long double		d;
 
 	i = 0;
-
 	d = 4.3f / 2.3f;
-	printf("\nTest #%d :\n", ++i);
+	printf("\nTest #%d %%#o:\n", ++i);
 	printf("| %-10s |\n", "printf");
-	printf("| %-10Lf |", d);
+	printf("| {%#o} |", 0);
 	printf("\n");
 	ft_printf("| %-10s |\n", "ft_printf");
-	ft_printf("| %-10Lf |", d);
+	ft_printf("| {%#o} |", 0);
+	ft_printf("\n");
+	printf("\nTest #%d %%#.o:\n", ++i);
+	printf("| %-10s |\n", "printf");
+	printf("%#.o %#.0o", 0, 0);
+	printf("\n");
+	ft_printf("| %-10s |\n", "ft_printf");
+	ft_printf("%#.o %#.0o", 0, 0);
+	ft_printf("\n");
+	printf("\nTest #%d %%.3o (1):\n", ++i);
+	printf("| %-10s |\n", "printf");
+	printf("%#.3o", 1);
+	printf("\n");
+	ft_printf("| %-10s |\n", "ft_printf");
+	ft_printf("%#.3o", 1);
+	ft_printf("\n");
+	printf("\nTest #%d %%.3o (0):\n", ++i);
+	printf("| %-10s |\n", "printf");
+	printf("%#.3o", 0);
+	printf("\n");
+	ft_printf("| %-10s |\n", "ft_printf");
+	ft_printf("%#.3o", 0);
 	ft_printf("\n");
 	return (0);
 }
