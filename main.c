6248f77e9c067c9df37c6ee865f790dec65fb92e
Stanley REGNARD <sregnard@e3r13p7.42.fr>
2019-02-14 14:07:33 +0100

    main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 17:26:00 by sregnard          #+#    #+#             */
-/*   Updated: 2019/02/13 21:02:26 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/02/14 13:02:45 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ int	main(int ac, char **av)
 	long double	d;
 
 	i = 0;
-	d = ULLONG_MAX + 1;
+	d = (double)ULLONG_MAX + 1;
 	printf("\nTest #%d :\n", ++i);
 	printf("| %-10s |\n", "printf");
-	printf("%010.1Lf", d);
+	printf("%010.p", &i);
 	printf("\n");
 	ft_printf("| %-10s |\n", "ft_printf");
-	ft_printf("%010.1Lf", d);
+	ft_printf("%010.p", &i);
 	ft_printf("\n");
 	return (0);
 }
