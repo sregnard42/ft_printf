2a46ee3c43780edfdf928216d5d09a4f02371c4f
Stanley REGNARD <sregnard@e2r4p7.42.fr>
2019-02-15 12:42:13 +0100

    main.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 17:26:00 by sregnard          #+#    #+#             */
-/*   Updated: 2019/02/14 13:02:45 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/02/15 12:41:44 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ int	main(int ac, char **av)
 	long double	d;
 
 	i = 0;
-	d = (double)ULLONG_MAX + 1;
+	d = LDBL_MIN;
 	printf("\nTest #%d :\n", ++i);
 	printf("| %-10s |\n", "printf");
-	printf("%010.p", &i);
+	printf("%-10Lf", d);
 	printf("\n");
 	ft_printf("| %-10s |\n", "ft_printf");
-	ft_printf("%010.p", &i);
+	ft_printf("%-10Lf", d);
 	ft_printf("\n");
 	return (0);
 }
